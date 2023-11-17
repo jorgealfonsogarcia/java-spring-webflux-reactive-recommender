4d3169c4f6700b34ee7dde34a06b07d26974b575
Jorge Garcia <jorgealfonsogarcia@gmail.com>
2023-11-17 18:52:36 -0500

    :sparkles: feat(actuator): Add health and info endpoints.

---
+++ src/main/java/com/jorgealfonsogarcia/recommender/monitor/AppVersionHealthIndicator.java
@@ -0,0 +1,57 @@
+/*
+ * MIT License
+ *
+ * COPYRIGHT (c) 2023 Jorge Garcia
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.jorgealfonsogarcia.recommender.monitor;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.stereotype.Component;
+
+/**
+ * A health indicator for the application version.
+ *
+ * @author Jorge Garcia
+ * @version 1.0.0
+ * @since 21
+ */
+@Component
+public class AppVersionHealthIndicator implements HealthIndicator {
+
+    private final String version;
+
+    /**
+     * Constructor.
+     *
+     * @param version The application version.
+     */
+    public AppVersionHealthIndicator(@Value("${app.version}") String version) {
+        this.version = version;
+    }
+
+    @Override
+    public Health health() {
+        return Health.up().withDetail("version", version).build();
+    }
+}
